d95d99ba9c10658a3f7aa8eec07e22e759d8ba75
ZofP <petr.zofak@gmail.com>
2025-04-06 21:37:15 +0200

    feat: add typography component

---
+++ src/components/Typography/utils/isHeadingVariant.ts
@@ -0,0 +1,7 @@
+import { TypographyHeadingVariant } from "../Typography.types";
+
+const allowedVariants = ["h1", "h2", "h3", "h4", "h5", "h6"];
+
+export const isHeadingVariant = (
+  variant: string
+): variant is TypographyHeadingVariant => allowedVariants.includes(variant);
