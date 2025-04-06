41ff63bb5d87446129a3440e78fdc7e85d430454
ZofP <petr.zofak@gmail.com>
2025-04-06 17:52:19 +0200

    feat(localization): add next-intl configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false,
 };
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin({
+  requestConfig: "./src/lib/i18n/request.ts",
+  experimental: {
+    /** A path to the messages file that you'd like to create a declaration for. In case you want to consider multiple files, you can pass an array of paths. */
+    createMessagesDeclaration: "./src/lib/i18n/messages/en.json",
+  },
+});
+export default withNextIntl(nextConfig);
