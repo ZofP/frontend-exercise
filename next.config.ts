e6574039fa6d85a5078c8eae005c3d4c924fae92
ZofP <petr.zofak@gmail.com>
2025-04-07 22:42:27 +0200

    build: add typescript checks of routes

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  experimental: {
+    typedRoutes: true,
+  },
 };
 
 const withNextIntl = createNextIntlPlugin({
