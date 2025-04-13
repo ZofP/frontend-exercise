5bd4aec9d64ca37ee0ee595495219444e33ffa1d
ZofP <petr.zofak@gmail.com>
2025-04-13 15:21:27 +0200

    feat: add fetch article by id action

---
+++ src/actions/article/fetchArticleById.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { CONFIG } from "@/config";
+import { ArticleDetail, articleDetailSchema } from "@/types/article";
+import { buildDynamicPath } from "@/utils/url";
+import { fetchBase } from "../helpers/fetchBase";
+
+export const fetchArticleById = async (
+  articleId: string
+): Promise<ArticleDetail> => {
+  const res = await fetchBase(
+    buildDynamicPath(CONFIG.api.endpoints.anonymous.articleById, { articleId }),
+    {
+      method: "GET",
+      next: { tags: [`article-${articleId}`] },
+    }
+  );
+
+  console.log({ res });
+
+  return articleDetailSchema.parse(res);
+};
--- src/actions/article/index.ts
+++ src/actions/article/index.ts
@@ -1,2 +1,3 @@
+export * from "./fetchArticleById";
 export * from "./fetchArticles";
 export * from "./fetchImage";
