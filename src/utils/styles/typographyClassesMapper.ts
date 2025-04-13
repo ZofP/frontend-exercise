7e50db64d0a54a37a36c55c81c23cc0108ccfa8a
ZofP <petr.zofak@gmail.com>
2025-04-13 16:06:25 +0200

    revert(style): keep typography classes mapping for Link component

--- src/utils/styles/typographyClassesMapper.ts
+++ src/utils/styles/typographyClassesMapper.ts
@@ -1,9 +1,12 @@
-import { TypographyParagraphVariant } from "@/types";
+import { TypographyVariant } from "@/types";
 
-export const typographyClassesMapper: Record<
-  TypographyParagraphVariant,
-  string
-> = {
+export const typographyClassesMapper: Record<TypographyVariant, string> = {
+  h1: "text-[40px] leading-48",
+  h2: "text-[32px] leading-38",
+  h3: "text-[28px] leading-32",
+  h4: "text-[24px] leading-28",
+  h5: "text-[20px] leading-24",
+  h6: "text-[16px] leading-20",
   "lead-blockquote": "text-[20px] leading-30",
   body: "text-[16px] leading-24",
   "body-bold": "text-[16px] leading-24 font-bold",
