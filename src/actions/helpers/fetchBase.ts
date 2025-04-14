aaf4e5f94dfd55ef65edf880526d2e6c6b47532d
ZofP <petr.zofak@gmail.com>
2025-04-14 21:54:01 +0200

    refactor: remove unnecessary code

--- src/actions/helpers/fetchBase.ts
+++ src/actions/helpers/fetchBase.ts
@@ -1,32 +1,27 @@
 "use server";
 
-import { cookies } from "next/headers";
-
 import { CONFIG } from "@/config";
-import { CookieKey, ExtendedRequestInit } from "@/types";
+import { ExtendedRequestInit } from "@/types";
+
+const DEFAULT_HEADERS: Record<string, string> = {
+  "Content-Type": "application/json",
+  "X-API-KEY": CONFIG.app.env.apiKey,
+};
 
 export const fetchBase = async (
   path: string | URL | Request,
   init?: ExtendedRequestInit
 ) => {
-  const token = (await cookies()).get(CookieKey.AccessToken)?.value;
-  const headers: Record<string, string> = {
-    "Content-Type": "application/json",
-    "X-API-KEY": CONFIG.app.env.apiKey,
-  };
-  if (token) {
-    headers.Authorization = `Bearer ${token}`;
-  }
   const fullUrl = `${CONFIG.app.env.apiUrl}${path}`;
 
   if (!init) {
-    return await fetch(fullUrl, { headers });
+    return await fetch(fullUrl, { headers: DEFAULT_HEADERS });
   }
 
   const { body, ...restInit } = init;
   const options: RequestInit = {
     ...restInit,
-    headers: { ...restInit.headers, ...headers },
+    headers: { ...restInit.headers, ...DEFAULT_HEADERS },
   };
   if (body) {
     options.body = JSON.stringify(body);
