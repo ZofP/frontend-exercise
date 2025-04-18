97ccf6d7389a6a0ca6891b3c68c1f4767104182f
ZofP <petr.zofak@gmail.com>
2025-04-18 15:42:05 +0200

    refactor: rename Link to TypographyLink

---
+++ src/components/TypographyLink/TypographyLink.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+import NextLink from "next/link";
+
+import { LinkProps } from "@/types";
+import { typographyClassesMapper } from "@/utils";
+
+export const TypographyLink = <T,>({
+  variant = "body",
+  className,
+  children,
+  ...props
+}: LinkProps<T>) => {
+  const appliedClasses = clsx(typographyClassesMapper[variant], className);
+  return (
+    <NextLink {...props} className={appliedClasses}>
+      {children}
+    </NextLink>
+  );
+};
